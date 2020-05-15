84f406935896839cc8fab26b7beeb60c000064dd
stackTom <rtiky23@gmail.com>
2020-05-15 19:31:15 -0400

    rm binary

--- gmp_decimal.c
+++ gmp_decimal.c
@@ -1,11 +1,11 @@
 #include "gmp_decimal.h"
 
-int mpq_set_decimal_str (mpz_t rop, const char *str, int base) {
+int mpq_set_decimal_str(mpz_t rop, const char *str, int base) {
     
     return 0;
 }
 
-char * mpq_get_decimal_str (char *str, int base, const mpq_t op) {
+char * mpq_get_decimal_str(char *str, int base, const mpq_t op) {
     
     return NULL;
 }
--- gmp_decimal.h
+++ gmp_decimal.h
@@ -1,7 +1,7 @@
 #ifndef GMP_DECIMAL_H
 #define GMP_DECIMAL_H
 #include "gmp.h"
-int mpq_set_decimal_str (mpz_t rop, const char *str, int base);
-char * mpq_get_decimal_str (char *str, int base, const mpq_t op);
+int mpq_set_decimal_str(mpz_t rop, const char *str, int base);
+char * mpq_get_decimal_str(char *str, int base, const mpq_t op);
 #endif
 
