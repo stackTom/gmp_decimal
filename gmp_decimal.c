941634115f8aa5e2aec19a0c0ef2920fe8bfcb3b
stackTom <rtiky23@gmail.com>
2020-05-16 05:42:26 +0000

    only 2 tokens in decimal, no need for while loop

--- gmp_decimal.c
+++ gmp_decimal.c
@@ -22,29 +22,26 @@ int mpq_set_decimal_str(mpq_t rop, const char *str, int base) {
     mpz_inits(fractional_part, numerator, denominator, NULL);
     token = strtok(src, PERIOD);
 
-    int first_token = 1;
-    while (token) {
-        if (first_token) {
-            first_token = 0;
-            mpz_set_str(numerator, token, base);
-        } else {
-            // second token (aka decimal part)
-            mpz_set_str(fractional_part, token, base);
-            // + 1 for extra 0 for proper denominator, + 1 for null terminator
-            size_t denominator_str_len = strlen(token) + 1;
-            char *denominator_str = malloc((denominator_str_len + 1)* sizeof(char));
-            denominator_str[0] = '1';
-            memset(denominator_str + 1, '0', denominator_str_len - 1);
-            denominator_str[denominator_str_len] = '\0';
-            mpz_set_str(denominator, denominator_str, base);
-            mpz_mul(numerator, denominator, numerator);
-            mpz_add(numerator, numerator, fractional_part);
-            free(denominator_str);
-        }
-        token = strtok(NULL, PERIOD);
-    }
+    // first token (aka integer part)
+    mpz_set_str(numerator, token, base);
+    token = strtok(NULL, PERIOD);
+
+    // second token (aka decimal part)
+    mpz_set_str(fractional_part, token, base);
+    // + 1 for extra 0 for proper denominator, + 1 for null terminator
+    size_t denominator_str_len = strlen(token) + 1;
+    char *denominator_str = malloc((denominator_str_len + 1)* sizeof(char));
+    denominator_str[0] = '1';
+    memset(denominator_str + 1, '0', denominator_str_len - 1);
+    denominator_str[denominator_str_len] = '\0';
+    mpz_set_str(denominator, denominator_str, base);
+    mpz_mul(numerator, denominator, numerator);
+    mpz_add(numerator, numerator, fractional_part);
+    free(denominator_str);
+
     char *numerator_str = mpz_get_str(NULL, 10, numerator);
-    char *denominator_str = mpz_get_str(NULL, 10, denominator);
+
+    denominator_str = mpz_get_str(NULL, 10, denominator);
     // + 1 for null terminator, +1 for "/"
     size_t numerator_str_len = strlen(numerator_str);
     size_t rational_str_len = numerator_str_len + strlen(denominator_str) + 1;
